b429f2842168528465f02351604128aabc972da5
Oliver Shearman <oj@ojs05.live>
2022-01-08 19:01:05 +1100

    polish and add postgres

--- app/src/main/java/ApplicationBot/ConfigManager.java
+++ app/src/main/java/ApplicationBot/ConfigManager.java
@@ -11,11 +11,11 @@ public class ConfigManager {
     public static final String USERNAME = "applications";
     public static final String PASSWORD = "B7s4T&dK";
 
-    public static final String APPLICATION_URL = "jdbc:postgresql://140.238.197.160:5432/applications";
+    public static final String APPLICATION_URL = "jdbc:postgresql://postgres:5432/applications";
 
-    public static final String WHITELIST_URL = "jdbc:postgresql://140.238.197.160:5432/whitelist";
+    public static final String WHITELIST_URL = "jdbc:postgresql://postgres:5432/whitelist";
 
-    public static final String ARCHIVE_URL = "jdbc:postgresql://140.238.197.160:5432/archive";
+    public static final String ARCHIVE_URL = "jdbc:postgresql://postgres:5432/archive";
 
     public static final String TOKEN = "ODk2NzI2MjkwMjU2NzY0OTc5.YWLTbw.w-IRGjbJ2sLfhF-Wgha7kGn6Wec";
     public static final Long CHANNEL_ID = 825281666636316672L;
