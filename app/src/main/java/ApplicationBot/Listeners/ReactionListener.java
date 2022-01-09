849d1c2b59f5e6efab3bfb4113caf995a48267a9
Oliver Shearman <oj@ojs05.live>
2022-01-09 20:23:27 +1100

    fix silly reaction check

--- app/src/main/java/ApplicationBot/Listeners/ReactionListener.java
+++ app/src/main/java/ApplicationBot/Listeners/ReactionListener.java
@@ -11,70 +11,71 @@ public class ReactionListener extends ListenerAdapter {
     private static int id = 0;
 
     public void onMessageReactionAdd(MessageReactionAddEvent event) {
+        if(event.getChannel().getId().equals("929570131980460042")){
+            event.retrieveMessage().queue(message -> {
+                id = Integer.parseInt(message.getEmbeds().get(0).getFooter().getText());
 
-        event.retrieveMessage().queue(message -> {
+                if (event.getReactionEmote().getAsCodepoints().equals("U+2705")) {
+                    try {
+                        if (event.getUser().isBot()) return;
+                        if (!DatabaseManager.hasVoted(id, event.getUser().getId())) {
+                            DatabaseManager.addVoteFor(id, event.getUser().getId());
+                        }
+                    } catch (SQLException e) {
+                        e.printStackTrace();
+                    }
+                    try {
+                        if (DatabaseManager.getVotesFor(id) >= 3) {
+                            DatabaseManager.acceptApplication(id);
+                        }
+                    } catch (SQLException e) {
+                        e.printStackTrace();
+                    }
+                }
 
-            id = Integer.parseInt(message.getEmbeds().get(0).getFooter().getText());
-
-            if (event.getReactionEmote().getAsCodepoints().equals("U+2705")) {
-                try {
-                    if (event.getUser().isBot()) return;
-                    if (!DatabaseManager.hasVoted(id, event.getUser().getId())) {
-                        DatabaseManager.addVoteFor(id, event.getUser().getId());
+                if (event.getReactionEmote().getAsCodepoints().equals("U+274c")) {
+                    try {
+                        if(event.getUser().isBot()) return;
+                        if (!DatabaseManager.hasVoted(id, event.getUser().getId())) {
+                            DatabaseManager.addVoteAgainst(id, event.getUser().getId());
+                        }
+                    } catch (SQLException e) {
+                        e.printStackTrace();
                     }
-                } catch (SQLException e) {
-                    e.printStackTrace();
-                }
-                try {
-                    if (DatabaseManager.getVotesFor(id) >= 3) {
-                        DatabaseManager.acceptApplication(id);
+                    try {
+                        if (DatabaseManager.getVotesAgainst(id) >= 3) {
+                            DatabaseManager.rejectApplication(id);
+                        }
+                    } catch (SQLException e) {
+                        e.printStackTrace();
                     }
-                } catch (SQLException e) {
-                    e.printStackTrace();
                 }
-            }
-
-            if (event.getReactionEmote().getAsCodepoints().equals("U+274c")) {
-                try {
-                    if(event.getUser().isBot()) return;
-                    if (!DatabaseManager.hasVoted(id, event.getUser().getId())) {
-                        DatabaseManager.addVoteAgainst(id, event.getUser().getId());
-                    }
-                } catch (SQLException e) {
-                    e.printStackTrace();
-                }
-                try {
-                    if (DatabaseManager.getVotesAgainst(id) >= 3) {
-                        DatabaseManager.rejectApplication(id);
-                    }
-                } catch (SQLException e) {
-                    e.printStackTrace();
-                }
-            }
-        });
+            });
+        }
     }
 
     public void onMessageReactionRemove(MessageReactionRemoveEvent event) {
+        if(event.getChannel().getId().equals("929570131980460042")){
+            event.retrieveMessage().queue(message -> {
+                id = Integer.parseInt(message.getEmbeds().get(0).getFooter().getText());
 
-        event.retrieveMessage().queue(message -> {
-            id = Integer.parseInt(message.getEmbeds().get(0).getFooter().getText());
-
-            if (event.getReactionEmote().getAsCodepoints().equals("U+2705")) {
-                try {
-                    DatabaseManager.removeVoteFor(id, event.getUser().getId());
-                } catch (SQLException e) {
-                    e.printStackTrace();
+                if (event.getReactionEmote().getAsCodepoints().equals("U+2705")) {
+                    try {
+                        DatabaseManager.removeVoteFor(id, event.getUser().getId());
+                    } catch (SQLException e) {
+                        e.printStackTrace();
+                    }
                 }
-            }
 
-            if (event.getReactionEmote().getAsCodepoints().equals("U+274c")) {
-                try {
-                    DatabaseManager.removeVoteAgainst(id, event.getUser().getId());
-                } catch (SQLException e) {
-                    e.printStackTrace();
+                if (event.getReactionEmote().getAsCodepoints().equals("U+274c")) {
+                    try {
+                        DatabaseManager.removeVoteAgainst(id, event.getUser().getId());
+                    } catch (SQLException e) {
+                        e.printStackTrace();
+                    }
                 }
-            }
-        });
+            });
+        }
     }
 }
 
