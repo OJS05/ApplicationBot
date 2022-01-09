ecd0ad5bf4f3b085329e09b0339307ae36a49436
Oliver Shearman <oj@ojs05.live>
2022-01-10 00:37:36 +1100

    am dumbass

--- app/src/main/java/ApplicationBot/Listeners/GuildEventListener.java
+++ app/src/main/java/ApplicationBot/Listeners/GuildEventListener.java
@@ -1,6 +1,7 @@
 package ApplicationBot.Listeners;
 
 import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.entities.Member;
 import net.dv8tion.jda.api.events.guild.member.GuildMemberJoinEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
@@ -8,10 +9,10 @@ public class GuildEventListener extends ListenerAdapter {
     @Override
     public void onGuildMemberJoin(GuildMemberJoinEvent event) {
         System.out.println("eventjoin");
-        String userId = event.getMember().getId();
+        Member user = event.getMember();
         long guildId = event.getGuild().getIdLong();
         JDA bot = event.getJDA();
-        bot.getGuildById(guildId).addRoleToMember(userId, bot.getRoleById(804551889395646464L)).queue();
-        bot.getUserById(userId).openPrivateChannel().queue(channel -> channel.sendMessage("Your ID for the application process is: " + event.getUser().getId()).queue());
+        bot.getGuildById(guildId).addRoleToMember(user.getId(), bot.getRoleById(804551889395646464L)).queue();
+        user.getUser().openPrivateChannel().queue(channel -> channel.sendMessage("Your ID for the application process is: " + event.getUser().getId()).queue());
     }
 }
